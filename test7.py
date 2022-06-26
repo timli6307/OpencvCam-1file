42af800a7707d78de0230d89b2e5d384129b7746
aplendidempire <timli6307@gmail.com>
2022-06-26 09:46:30 +0800

    test2

--- test7.py
+++ test7.py
@@ -6,7 +6,7 @@ import sys
 COM_PORT = 'COM11'  # 請自行修改序列埠名稱
 BAUD_RATES = 9600
 ser = serial.Serial(COM_PORT, BAUD_RATES)
-w="weee"
+w="wee"
 while(True):
     ser.write(b'weee\n',)  # 訊息必須是位元組類型
     sleep(0.5)
